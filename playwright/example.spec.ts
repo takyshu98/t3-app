a391045939759be084868c0534032aab7f953ea9
takyshu98 <shuhei.takiguchi.0908@gmail.com>
2024-08-19 20:47:56 +0900

    mod github actions setting

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -3,8 +3,6 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  console.log('Page URL:', page.url());
-
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Create T3 App/);
 });
