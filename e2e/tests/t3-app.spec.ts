3e1fcff4e254dc8f6727e7a2c7f5be69b868b8c7
takyshu98 <shuhei.takiguchi.0908@gmail.com>
2024-08-23 22:03:55 +0900

    mod github actions setting

--- e2e/tests/t3-app.spec.ts
+++ e2e/tests/t3-app.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { PrismaClient } from "@prisma/client";
 
 test('has title', async ({ page }) => {
   await page.goto('/');
@@ -8,10 +9,49 @@ test('has title', async ({ page }) => {
 });
 
 test("ログイン状態でアクセスすると、ユーザ情報が表示される", async ({
-  page,
+  browser,
 }) => {
-  const cookie = await page.context().cookies('http://localhost')
-  console.log(cookie)
+  // const cookie = await page.context().cookies('http://localhost')
+  // console.log(cookie)
+  const prisma = new PrismaClient();
+  // あらかじめSessionテーブルにテストユーザと紐づくレコードを作成しておく
+  const existingRecord = await prisma.session.findUnique({
+    where: {
+      sessionToken: "dummy"
+    },
+  });
+    
+  if (existingRecord) {
+    await prisma.session.delete({
+      where: {
+        sessionToken: "dummy"
+      },
+    });
+  }
+
+  await prisma.session.create({
+    data: {
+      sessionToken: "dummy",
+      userId: "1",
+      expires: new Date(new Date().getTime() + 86400),
+    },
+  });
+
+  const context = await browser.newContext();
+  await context.addCookies([
+    {
+      name: 'next-auth.session-token',
+      value: 'dummy',
+      domain: 'localhost:3000',
+      path: '/',
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+    },
+  ]);
+
+  // ログインしていないとアクセスできないページのテストをする
+  const page = await context.newPage();
+
   await page.goto('/');
   // await page.getByRole('link', { name: /Sign in/i }).click();
   await page.waitForTimeout(1000);
